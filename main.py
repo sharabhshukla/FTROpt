689003c5fbb08e800ff960860f2f9c3627670923
Sharabh Shukla <sharabh2019@gmail.com>
2024-02-19 12:44:39 +0530

    Add comments and docstrings to FTR optimizer code
    The code is updated to include comments and docstrings for better readability and understanding. This is done for all the methods and various constraints in the src/model/ftr_opt.py and main.py files. The commit also changes some variable descriptions for better clarity, like changing the description "Binary variable indicating if a path is selected" to "Binary variable indicating if an arc is selected".

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 from src.model.ftr_opt import FTROpt
 
-
+# Read all files
 z_bcl_df = pd.read_csv("./data/raw/Z_BCLimits.csv")
 z_cost_df = pd.read_csv("./data/raw/Z_Cost.csv")
 z_rev_df = pd.read_csv("./data/raw/Z_Rev.csv")
 shift_fctrs_df = pd.read_csv("./data/raw/Z_SF.csv")
 path_pref_df = pd.read_csv("./data/raw/Z_MPUnSym.csv")
 
-
+# Initialize the FTR portfolio optimizer class
 ftr_opt = FTROpt(
     z_cost=z_cost_df,
     z_bcl=z_bcl_df,
@@ -17,4 +17,6 @@ ftr_opt = FTROpt(
     path_pref=path_pref_df
 )
 
+
+# Run the FTR portfolio optimizer
 ftr_opt.optimize_portfolio()
--- src/model/ftr_opt.py
+++ src/model/ftr_opt.py
@@ -14,6 +14,11 @@ class FTROpt:
     path_pref: pd.DataFrame
 
     def __post_init__(self):
+        """
+        Method to perform post-initialization operations.
+
+        :return: None
+        """
         self.nodes = self.z_cost.columns.tolist()
         self.z_cost.index = self.nodes
         self.z_rev.index = self.nodes
@@ -21,6 +26,18 @@ class FTROpt:
         self.path_pref.index = self.path_pref.columns.tolist()
 
     def optimize_portfolio(self):
+        """
+        Optimize Portfolio
+
+        This method optimizes the allocation of MW (MegaWatt) along different paths in order to maximize net revenue. It uses a linear optimization model to find the optimal solution.
+
+        Parameters:
+            None
+
+        Returns:
+            None
+
+        """
         model = ConcreteModel()
 
         nodes = self.z_cost.columns.tolist()
@@ -36,6 +53,13 @@ class FTROpt:
 
         # Shift Factors
         def extract_shift_factors(model, node, constraint):
+            """
+            :param model: The model to extract the shift factors from.
+            :param node: The node to extract the shift factors for.
+            :param constraint: The constraint to extract the shift factors for.
+            :return: The shift factors for the given node and constraint.
+
+            """
             return self.shift_factors.loc[self.shift_factors.index == node, constraint].iloc[0]
 
         model.shift_factors = Param(model.nodes, model.decomp_limits,
@@ -44,9 +68,22 @@ class FTROpt:
 
         # Binding Constraints
         def z_bc_min_limits(model, constraint):
+            """
+            :param model: The model used for optimization.
+            :param constraint: The constraint for which to find the minimum limit.
+            :return: The minimum limit value for the given constraint.
+
+            """
             return self.z_bcl.loc[self.z_bcl['Mas_Cons'] == constraint, 'MasMin'].iloc[0]
 
         def z_bc_max_limits(model, constraint):
+            """
+            Retrieves the maximum limit from the FTROpt.z_bcl data frame for a specific constraint.
+
+            :param model: The FTROpt model object.
+            :param constraint: The constraint for which the maximum limit needs to be retrieved.
+            :return: The maximum limit value.
+            """
             return self.z_bcl.loc[self.z_bcl['Mas_Cons'] == constraint, 'MasMax'].iloc[0]
 
         model.bc_max = Param(model.decomp_limits, initialize=lambda model, c: z_bc_max_limits(model, c),
@@ -54,7 +91,7 @@ class FTROpt:
         model.bc_min = Param(model.decomp_limits, initialize=lambda model, c: z_bc_min_limits(model, c),
                              within=Reals)
 
-        # Binary variable indicating if a path is selected
+        # Binary variable indicating if a arc is selected
         model.arc_indicator = Var(model.arcs, within=Binary)
 
         # MW allocated to each arc, path between node i and node j
@@ -65,6 +102,11 @@ class FTROpt:
 
         # Decomposition constraints
         def decomposition_rule_min(model, constraint):
+            """
+            :param model: The optimization model.
+            :param constraint: The constraint for which the decomposition rule should be applied.
+            :return: True if the sum of allocated_mw multiplied by the difference of shift factors for each arc in the model meets the minimum constraint value.
+            """
             return sum(
                 model.allocated_mw[i, j] * (
                         model.shift_factors[i, constraint] - model.shift_factors[j, constraint]) for
@@ -73,6 +115,11 @@ class FTROpt:
         model.decomp_min_limit = Constraint(model.decomp_limits, rule=decomposition_rule_min)
 
         def decomposition_rule_max(model, constraint):
+            """
+            :param model: The optimization model
+            :param constraint: The constraint for maximum shift factor difference
+            :return: True if the decomposition rule is satisfied, False otherwise
+            """
             return sum(model.allocated_mw[i, j] * (
                     model.shift_factors[i, constraint] - model.shift_factors[j, constraint]) for i, j in
                        model.arcs) <= model.bc_max[constraint]
@@ -80,17 +127,42 @@ class FTROpt:
         model.decomp_max_limit = Constraint(model.decomp_limits, rule=decomposition_rule_max)
 
         def min_allocated_mw_constraint(model, i, j):
-            return model.allocated_mw[i, j] <= model.arc_indicator[i, j] * 50
+            """
+            Minimizes the allocation of MW (megawatts) constraint for a given model, arc indices i and j.
+
+            :param model: The optimization model.
+            :type model: FTROpt
+            :param i: The index of the source arc.
+            :type i: int
+            :param j: The index of the destination arc.
+            :type j: int
+            :return: The constraint that ensures the allocated MW is less than or equal to the arc indicator multiplied by 50.
+            :rtype: constraint
+            """
+            return model.allocated_mw[i, j] <= model.arc_indicator[i, j] * 50 # 50 is max MW limit for each arc
 
         model.min_allocated_mw_constraint = Constraint(model.arcs, rule=min_allocated_mw_constraint)
 
         def path_pref_objective(model):
+            """
+            :param model: The optimization model containing necessary variables and parameters for portfolio optimization.
+
+            :return: The path preference objective value, which is calculated by summing the product of the arc weights (path_pref) and the arc indicators (arc_indicator) over all arcs in the model
+            *.
+
+            """
             return model.portfolio_path_pref == sum(model.path_pref[i, j] * model.arc_indicator[i, j] for i, j in model.arcs)
 
         model.path_pref_cons = Constraint(rule=path_pref_objective)
 
         # Step 1: Solve for the first objective (net revenue)
         def objective_revenue(model):
+            """
+            :param model: An instance of the FTROpt class representing the optimization portfolio model.
+            :return: The objective revenue calculated based on the model's allocated_mw, revs, costs, and portfolio_path_pref attributes.
+            The objective is weighted 0.9 for total revenue and 0.1 10% for path preferences
+
+            """
             return (0.9*sum(model.allocated_mw[i, j] * (model.revs[i, j] - model.costs[i, j]) for i, j in model.arcs) + 0.1*
                     model.portfolio_path_pref)
 
